50658cc5a359f9cd9043dc193c8bf6242970a16d
Ahmed Noor Kader Mustajir Md Eusoff <sir.ade@gmail.com>
2024-10-01 11:21:58 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,3 +1,3 @@
 <?php
-echo "<p><center>Copyright &copy; 2020-" . date("Y") . " <a href='http://mustajir.org'>Kader</a><center></p>";
+echo "<p><center>Copyright &copy; 2020-" . date("Y") . " <a href='http://mustajir.org'>Ahmed Noor Kader Mustajir</a><center></p>";
 ?>
